031dbda2ddc469246b5900a06f2d17a52ae770c1
Nico Braun <rainbowstack@gmail.com>
2023-07-25 22:14:27 +0200

    ci: release pipeline
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>

--- .github/release.sh
+++ .github/release.sh
@@ -16,7 +16,7 @@ rid="$(curl -fsSL \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/bluebrown/vscode-extension-yamlfmt/releases \
+  https://api.github.com/repos/bluebrown/kobold/releases \
   -d '{ "tag_name":"'"$VERSION"'", "generate_release_notes": true, "prerelease": '"${pre:-false}"' }' |
   jq -r '.id')"
 
